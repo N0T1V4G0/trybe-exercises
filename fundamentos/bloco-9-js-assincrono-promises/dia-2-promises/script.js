5dcfcd373fab0e509e68fd868fd304df8035e8eb
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-10-18 14:54:10 -0300

    add code de fixação

---
+++ fundamentos/bloco-9-js-assincrono-promises/dia-2-promises/script.js
@@ -0,0 +1,52 @@
+const jsonInfo = `{
+  "muitasEmpresasUsam": [
+    "Google",
+    "Twitter",
+    "Facebook",
+    "etc..."
+  ],
+  "temVariasVantagens": [
+    "Legível",
+    "Fácil de usar",
+    "Leve",
+    "Popular",
+    "Versátil"
+  ],
+  "muitasLinguagensDaoSuporte": [
+    "Python",
+    "C",
+    "C++",
+    "Java",
+    "PHP"
+  ]
+}`;
+
+const usoJSONPorque = JSON.parse(jsonInfo);
+// console.log(usoJSONPorque);
+
+const corporationsList = document.getElementById('corporations-used-by');
+const advantagesList = document.getElementById('advantages');
+const languagesList = document.getElementById('languages-used-by');
+
+usoJSONPorque.muitasEmpresasUsam.map((empresa) => {
+  const newLi = document.createElement('li');
+  newLi.innerText = empresa;
+  corporationsList.appendChild(newLi);
+});
+
+usoJSONPorque.temVariasVantagens.map((vantagens) => {
+  const newLi = document.createElement('li');
+  newLi.innerText = vantagens;
+  advantagesList.appendChild(newLi);
+});
+
+usoJSONPorque.muitasLinguagensDaoSuporte.map((linguagens) => {
+  const newLi = document.createElement('li');
+  newLi.innerText = linguagens;
+  languagesList.appendChild(newLi);
+});
+
+fetch('https://api.chucknorris.io/jokes/random?category=dev')
+  .then((response) => response.json())
+  .then((data) => console.log(data.value))
+  .catch((error) => console.log(`Algo deu errado: ${error}`));
