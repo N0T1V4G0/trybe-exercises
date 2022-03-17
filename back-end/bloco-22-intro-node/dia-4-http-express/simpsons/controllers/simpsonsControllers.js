8f76ef70fa2ce38da182d8e54d2467c2e3d5719c
Lucas Barbosa <lukeb.trybe@gmail.com>
2022-03-17 18:54:55 -0300

    feat: adiciona getSimpson e checkID nos controllers

--- back-end/bloco-22-intro-node/dia-4-http-express/simpsons/controllers/simpsonsControllers.js
+++ back-end/bloco-22-intro-node/dia-4-http-express/simpsons/controllers/simpsonsControllers.js
@@ -3,7 +3,29 @@ const fs = require('fs');
 const FILEPATH = `${__dirname}/../data/simpsons.json`;
 const data = JSON.parse(fs.readFileSync(FILEPATH, 'utf-8'));
 
-exports.getAllSimpsons = (req, res, next) => {
+exports.checkID = (req, res, next) => {
+  const id = data.find((el) => el.id === req.params.id);
+  if (!id) {
+    return res.status(404).json({
+      status: 'not found',
+      message: 'invalid ID',
+    });
+  }
+  next();
+};
+
+exports.getSimpson = (req, res) => {
+  const simpson = data.find((el) => el.id === req.params.id);
+  return res.status(200).json({
+    status: 'ok',
+    message: 'success',
+    data: {
+      simpson,
+    },
+  });
+};
+
+exports.getAllSimpsons = (req, res) => {
   return res.status(200).json({
     status: 'success',
     data: {
--- back-end/bloco-22-intro-node/dia-4-http-express/simpsons/routes/simpsonsRoutes.js
+++ back-end/bloco-22-intro-node/dia-4-http-express/simpsons/routes/simpsonsRoutes.js
@@ -4,5 +4,8 @@ const simpsonsControllers = require('../controllers/simpsonsControllers');
 const router = express.Router();
 
 router.route('/').get(simpsonsControllers.getAllSimpsons);
+router
+  .route('/:id')
+  .get(simpsonsControllers.checkID, simpsonsControllers.getSimpson);
 
 module.exports = router;
